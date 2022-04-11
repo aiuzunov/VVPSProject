7f74286374de66107ba9c0d083b87e055e5d7991
aiuzunov <aleksandar.i.uzunov@gmail.com>
2022-04-11 17:09:10 +0300

    tests v2 commit

--- MainApp/tests.py
+++ MainApp/tests.py
@@ -4,9 +4,11 @@ import pandas
 # Create your tests here.
 
 
-class UnitTests(TestCase):
+class CorrectDataTests(TestCase):
     def setUp(self):
-        f = open("/home/alex/Documents/Logs_Course A_StudentsActivities.xlsx", "rb")
+        #f = open("/home/alex/Documents/Logs_Course A_StudentsActivities.xlsx", "rb")
+        f = open("/home/alex/Documents/Copy of Logs_Course A_StudentsActivities_invalid.xlsx", "rb")
+
         vv = pandas.read_excel(f)
         self.parser = FileParser.FileParser(vv);
         self.parser.findColsIndexes()
@@ -18,8 +20,24 @@ class UnitTests(TestCase):
 
 
     def test_calculate_mode(self):
-        self.assertEqual(self.parser.calculateMode(), [1])
+        self.assertEqual(self.parser.calculateMode(), [2])
 
 
     def test_standard_deviation(self):
         self.assertEqual(self.parser.calculateStandardDeviation(), 0)
+
+    #def test_frequencies(self):
+        #self.assertEqual(self.parser.calculateFrequencies(),{'8441': [2,0.5]})
+
+
+class InvalidDataTests(TestCase):
+    def setUp(self):
+        f = open("/home/alex/Documents/Copy of Logs_Course A_StudentsActivities_invalid.xlsx", "rb")
+        vv = pandas.read_excel(f)
+        self.parser = FileParser.FileParser(vv);
+        self.parser.findColsIndexes()
+        self.parser.getUsersSubmissions()
+
+    def test_calculate_mode(self):
+        print(self.parser.calculateFrequencies()['8411'])
+        self.assertEqual(self.parser.calculateMode(), [1])
